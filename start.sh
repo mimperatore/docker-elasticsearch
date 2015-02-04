fb6f2c2d47dc379dfd987e3030ef79d2bee72b1d
mimperatore@gmail.com <mimperatore@gmail.com>
2015-02-04 08:25:42 -0500

    change config file location

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ publish_host=$3
 transport_publish_port=$4
 unicast_hosts=$5
 
-/elasticsearch/bin/elasticsearch -Des.config=/config/elasticsearch.yml --cluster.name=$cluster_name --node.name=$node_name --network.publish_host=$publish_host --transport.publish_port=$transport_publish_port --discovery.zen.ping.multicast.enabled=false --discovery.zen.ping.unicast.hosts=$unicast_hosts
+/elasticsearch/bin/elasticsearch -Des.config=/elasticsearch/config/elasticsearch.yml --cluster.name=$cluster_name --node.name=$node_name --network.publish_host=$publish_host --transport.publish_port=$transport_publish_port --discovery.zen.ping.multicast.enabled=false --discovery.zen.ping.unicast.hosts=$unicast_hosts
